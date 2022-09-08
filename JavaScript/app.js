f7405dc66fae742115a6005b85acead2b78fba38
Joey Cos <joeycosentino24@gmail.com>
2022-09-08 16:10:00 -0400

    completed app.js section

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -5,3 +5,19 @@
 
 const colours = ['blue', 'red', 'purple', 'orange', 'green', 'yellow', 'pink', 'indigo'];
 
+const colour = document.querySelector('.colour');
+const button = document.getElementById('btn');
+
+button.addEventListener('click', function() {
+    // click button, change text
+    // text already has something
+
+    let newColour = colours[randomColour()];
+
+    colour.textContent = newColour;
+    document.body.style.backgroundColor = newColour;
+})
+
+function randomColour() {
+    return Math.floor(Math.random() * colours.length)
+}
