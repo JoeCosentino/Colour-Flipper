e1d85c0762ba6a2cf278e70932b99cedbf9a8f5c
Joey Cos <joeycosentino24@gmail.com>
2022-09-08 15:54:20 -0400

    added hex.js and hex.html file

---
+++ JavaScript/app.js
@@ -0,0 +1,7 @@
+// list of colours to use
+// dynamically change them
+// apply them, and change text
+// select them randomly through the button
+
+const colours = ['blue', 'red', 'purple', 'orange', 'green', 'yellow', 'pink', 'indigo'];
+
